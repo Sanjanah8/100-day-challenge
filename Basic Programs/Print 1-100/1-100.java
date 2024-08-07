af2e9230e79852642b36c4825f0d5c3c2f2fc195
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-07 21:14:54 +0530

    Create 1-100.java

---
+++ Basic Programs/Print 1-100/1-100.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.print(num1 - num2);
+	}
+}
