060fef6e519cf74df28e24d3ffc4d9eade84b95f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-07 21:21:34 +0530

    Create 111 to 2.java

---
+++ Basic Programs/print 111 to 2/111 to 2.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Hello {
+    public static void main(String[] args) {
+        for (int ctr = 111; ctr >= 2; ctr--) {
+            System.out.print(ctr + " ");
+        }
+    }
+}
