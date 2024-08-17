310c68c75d48a28a72ff937fafbc36ede27b48c5
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 10:00:25 +0530

    Delete Basic 100/Java/summ of first 10 natural numbers.java

--- Basic 100/Java/summ of first 10 natural numbers.java
+++
@@ -1,9 +0,0 @@
-public class SumFirst10Natural {
-    public static void main(String[] args) {
-        int sum = 0;
-        for (int i = 1; i <= 10; i++) {
-            sum += i;
-        }
-        System.out.println("Sum of first 10 natural numbers: " + sum);
-    }
-}
