5b4a0ce65fba31b9d86a229d4707a814773bd4f4
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:32:59 +0530

    Create perfect number.java

---
+++ Basic 100/Java/perfect number.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class PerfectNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int sum = 0;
+        for (int i = 1; i < num; i++) {
+            if (num % i == 0) {
+                sum += i;
+            }
+        }
+        if (sum == num) {
+            System.out.println(num + " is a perfect number.");
+        } else {
+            System.out.println(num + " is not a perfect number.");
+        }
+    }
+}
