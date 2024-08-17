b0edc5c4ec083215b9a3cc7df04badeff84c1a71
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 10:06:47 +0530

    Create anagram.java

---
+++ Basic 100/Java/anagram.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class AnagramCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
+}
