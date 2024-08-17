49c49a1e559eb92cf9fad46c49be19eaa46df66e
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:18:17 +0530

    Create Product of two numbers.java

---
+++ Basic 100/Java/Product of two numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int product = num1 * num2;
+        System.out.println("Product: " + product);
+    }
+}
