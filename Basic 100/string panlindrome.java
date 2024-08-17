1ff2bc6e5c2dbe3926d02a99d09fa50878636cfc
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:44:59 +0530

    Create string panlindrome.java

---
+++ Basic 100/string panlindrome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(reversed)) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
