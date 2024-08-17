6bf46a030511e14e2ec1a20a730b4accfdc499c1
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:35:16 +0530

    Create table of a number.java

---
+++ Basic 100/Java/table of a number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
