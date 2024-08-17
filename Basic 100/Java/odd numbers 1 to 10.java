8a29797249b1cec33106f50b5545e51bea247ec1
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:56:34 +0530

    Create odd numbers 1 to 10.java

---
+++ Basic 100/Java/odd numbers 1 to 10.java
@@ -0,0 +1,9 @@
+public class PrintOdd1To10 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
