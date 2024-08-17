88f2625224a7fa4d0ae580b0146ef48d854f9a33
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:23:30 +0530

    Update square of numbers.java

--- Basic 100/Java/square of numbers.java
+++ Basic 100/Java/square of numbers.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
+public class Square {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int square = num * num;
+        System.out.println("Square: " + square);
+    }
+}
 
