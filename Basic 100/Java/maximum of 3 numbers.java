c5abbfb6deff5a77e9b4e2ff5da42a3c507222c0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:53:57 +0530

    Create maximum of 3 numbers.java

---
+++ Basic 100/Java/maximum of 3 numbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+        int max = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+        System.out.println("The maximum is " + max);
+    }
+}
