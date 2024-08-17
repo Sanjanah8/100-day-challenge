c7abb9f68e3c4e31ff87c9301243b12fe24cbc5c
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:15:36 +0530

    Create Sum of two numbers.java

---
+++ Basic 100/Java/Sum of two numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum: " + sum);
+    }
+}
+
