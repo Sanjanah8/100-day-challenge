39220274346b44e45c891c110fd47c00d3a216d6
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 09:31:04 +0530

    Create positive or negative palindrome.java

---
+++ Basic 100/Java/positive or negative palindrome.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class PositiveNegative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println(num + " is positive.");
+        } else if (num < 0) {
+            System.out.println(num + " is negative.");
+        } else {
+            System.out.println(num + " is zero.");
+        }
+    }
+}
