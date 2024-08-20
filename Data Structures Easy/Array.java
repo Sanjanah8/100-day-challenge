db1f8cfb976b2c64681774a348c736919c802e8a
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-20 18:11:23 +0530

    Create Array.java

---
+++ Data Structures Easy/Array.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
